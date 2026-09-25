0f32255ec69a3fe5ac57e31b958bfcb56bb66068
Ken Morris <148256019+drkenmorris@users.noreply.github.com>
2026-09-24 21:29:45 -0500

    Add lib/supabase/config.ts

---
+++ lib/supabase/config.ts
@@ -0,0 +1,6 @@
+export const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ?? "https://ufyopuunkprfpcprfngk.supabase.co";
+
+export const supabasePublishableKey =
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ??
+  "sb_publishable_maYvS_uR0p8kd18I7h_PBg_PkeRBW4t";
